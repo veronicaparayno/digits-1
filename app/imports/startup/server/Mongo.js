ecb0cb6730c297d3519ec5dafd2260467f176ff2
Veronica Parayno <vparayn2@hawaii.edu>
2024-04-03 16:39:59 -1000

    cleaned up

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,22 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Contacts } from '../../api/contact/Contacts';
 
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
 
 // Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
 const addContacts = (contact) => {
   console.log(`  Adding: ${contact.lastName} (${contact.owner})`);
   Contacts.collection.insert(contact);
